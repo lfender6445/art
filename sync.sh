28809e5d3094103eba5969ba320321d359fe537e
Luke Fender <lfender6445@gmail.com>
2020-10-10 14:38:28 -0400

    cron sync - Sat Oct 10 14:38:28 EDT 2020

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd ~/source/art
 echo 'starting sync'
 git pull --rebase
-python make_readme.py
+/usr/local/bin/python3 make_readme.py
 git add .
 msg="cron sync - $(/bin/date)"
 git commit -m "${msg}"
